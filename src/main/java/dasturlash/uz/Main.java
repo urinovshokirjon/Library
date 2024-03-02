59fb3b018eaa2159a19240d4dd88bdeed097d9c4
Urinov Shokirjon <urinovshokirjon93@gmail.com>
2024-03-02 10:00:02 +0500

    Hibrnet qo'shil

--- src/main/java/dasturlash/uz/Main.java
+++ src/main/java/dasturlash/uz/Main.java
@@ -1,12 +1,26 @@
 package dasturlash.uz;
 import dasturlash.uz.controller.MainController;
+import org.hibernate.Hibernate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
+        Hibernate
+                asfdsa
+                fds
+                        af
+                        sa
+                                f
+                                s
+                                        fs
+                                        f
+                                                as
 
+                                                f
+                                                        s
+                                                        af
         ApplicationContext context=new ClassPathXmlApplicationContext("spring-core.xml");
         MainController mainController = context.getBean("mainController", MainController.class);
         mainController.start();
